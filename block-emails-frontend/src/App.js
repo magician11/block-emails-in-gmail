6cc71021755f342d718cda6ecfcd0c95e3c1d959
Andrew Golightly <support@andrewgolightly.com>
2017-10-27 23:40:08 +0100

    removed bootstrap

--- block-emails-frontend/src/App.js
+++ block-emails-frontend/src/App.js
@@ -1,61 +1,44 @@
 import React, { Component } from "react";
-import { Grid, Row, Col, Button } from "react-bootstrap";
-
-import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <Grid className="content">
-        <Row>
-          <Col xsOffset={8} xs={1} smOffset={10} className="toolbar">
-            <Button bsStyle="primary">Login</Button>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12} className="header">
+      <div>
+        <div className="toolbar">
+          <button className="menu-link">Login</button>
+        </div>
+        <div className="container">
+          <div className="header">
             <h1>Gmail Blocker</h1>
-            <p className="lead">Permanently Block Unwanted Emails</p>
-            <Button bsSize="large" bsStyle="success">
-              Sign Up Now
-            </Button>
-          </Col>
-        </Row>
-        <Row className="details">
-          <Col xs={12}>
-            <hr />
-          </Col>
-          <Col xs={12} sm={10}>
+            <p>Permanently Block Unwanted Emails</p>
+            <button>Sign Up Now</button>
+          </div>
+        </div>
+        <div className="container details-container">
+          <div className="details">
             <h2>Why use this?</h2>
-            <p className="lead">
+            <p>
               Gmail currently offers a way to automatically move certain emails
               to the Trash or Spam. This isn't good enough if you are tempted to
               check those folders. This service will instantly and permanently
               delete emails from certain email addresses.
             </p>
             <h2>Sooo... like when would I use this?</h2>
-            <p className="lead">
+            <p>
               Ever blocked an ex and kept checking your Trash or Spam in Gmail?
               This will make sure that any messages they send you will be
               instantly gone forever. Temptation removed :)
             </p>
-          </Col>
-        </Row>
-        <Row className="footer">
-          <Col xs={12}>
-            <hr />
-          </Col>
-          <Col xs={6}>
-            <p>&copy; {new Date().getFullYear()} Golightly+</p>
-          </Col>
-          <Col xs={6}>
-            <p className="text-right">
-              <a href="https://golightlyplus.com/contact/">Contact</a>
-            </p>
-          </Col>
-        </Row>
-      </Grid>
+          </div>
+        </div>
+        <div className="footer">
+          <p>&copy; {new Date().getFullYear()} Golightly+</p>
+          <p>
+            <a href="https://golightlyplus.com/contact/">Contact</a>
+          </p>
+        </div>
+      </div>
     );
   }
 }
