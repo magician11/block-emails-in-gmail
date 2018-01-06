50409cc557a850685c816e5479575b2e2587a40f
Andrew Golightly <support@andrewgolightly.com>
2018-01-06 22:06:38 +1300

    cleaned up logic on landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -4,49 +4,41 @@ import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
 import { withStyles } from 'material-ui/styles';
 import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
-import Loader from './Loader';
 import { Link } from 'react-router-dom';
 import Mops from '../images/mops.jpg';
 import styles from '../styling';
 
 const LandingPage = props => {
-  let content;
-  if (props.user) {
-    content = (
-      <Card>
-        <CardMedia
-          image={Mops}
-          title="Gmail Blocker"
-          className={props.classes.mediaImage}
-        />
-        <CardContent>
-          <Typography type="display2">Gmail Blocker</Typography>
-          <Typography type="headline" gutterBottom>
-            Frequently Clear Unwanted Emails
-          </Typography>
-          <Typography>
-            Gmail automatically clears your Bin folder only every 30 days. Gmail
-            Blocker improves on that by doing it every hour.
-          </Typography>
-        </CardContent>
+  return (
+    <Card>
+      <CardMedia
+        image={Mops}
+        title="Gmail Blocker"
+        className={props.classes.mediaImage}
+      />
+      <CardContent>
+        <Typography type="display2">Gmail Blocker</Typography>
+        <Typography type="headline" gutterBottom>
+          Frequently Clear Unwanted Emails
+        </Typography>
+        <Typography>
+          Gmail automatically clears your Bin folder only every 30 days. Gmail
+          Blocker improves on that by doing it every hour.
+        </Typography>
+      </CardContent>
 
-        <CardActions>
-          {!props.user && (
-            <Button dense color="primary" href="/auth/google">
-              Sign In
-            </Button>
-          )}
-          <Button dense color="primary" component={Link} to="/faq">
-            Learn More
+      <CardActions>
+        {!props.user && (
+          <Button dense color="primary" href="/auth/google">
+            Sign In
           </Button>
-        </CardActions>
-      </Card>
-    );
-  } else {
-    content = <Loader />;
-  }
-
-  return content;
+        )}
+        <Button dense color="primary" component={Link} to="/faq">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };
 
 const mapStateToProps = ({ user }) => {
