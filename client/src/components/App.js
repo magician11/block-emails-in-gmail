da2dc428c723dd6deb1ad91f0ad0d467787d6be2
Andrew Golightly <support@andrewgolightly.com>
2018-01-10 00:01:44 +1300

    added google analytics

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import ReactGA from 'react-ga';
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 import { withStyles } from 'material-ui/styles';
 import Reboot from 'material-ui/Reboot';
@@ -19,6 +20,11 @@ import * as actions from '../actions';
 const theme = createMuiTheme();
 
 class App extends Component {
+  constructor() {
+    super();
+    ReactGA.initialize('UA-63340534-5');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
   componentDidMount() {
     this.props.fetchUser();
   }
