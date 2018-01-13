4d4c15f82a859df367faf9e593e2ba667746f913
Andrew Golightly <support@andrewgolightly.com>
2018-01-13 18:00:07 +1300

    added encryption on server

--- loveyourself-api.js
+++ loveyourself-api.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const cookieSession = require('cookie-session');
 const bodyParser = require('body-parser');
 const { initialiseDatabase } = require('./services/mongoose');
@@ -32,8 +34,19 @@ const passport = require('passport');
 
   require('./routes/index')(app);
 
-  // handling routes for the front end
+  const PORT = process.env.PORT || 8008;
+  const serverStartMessage = `Love Yourself server started on port ${
+    PORT
+  } at ${new Date().toString()}`;
+
   if (process.env.NODE_ENV === 'production') {
+    const sslBase = '/etc/letsencrypt/live/loveyourself.io';
+    const sslOptions = {
+      key: fs.readFileSync(`${sslBase}/privkey.pem`),
+      cert: fs.readFileSync(`${sslBase}/fullchain.pem`),
+      ca: fs.readFileSync(`${sslBase}/chain.pem`)
+    };
+
     app.use(express.static('client/build'));
     const path = require('path');
 
@@ -41,10 +54,14 @@ const passport = require('passport');
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-  }
 
-  const PORT = process.env.PORT || 8008;
-  app.listen(PORT, () => {
-    console.log(`Love Yourself server started on port ${PORT}`);
-  });
+    // startup the https server
+    https.createServer(sslOptions, app).listen(port, () => {
+      console.log(serverStartMessage);
+    });
+  } else {
+    app.listen(PORT, () => {
+      console.log(serverStartMessage);
+    });
+  }
 })();
