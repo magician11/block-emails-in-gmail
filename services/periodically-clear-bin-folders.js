141e50c30f802899e4a9ee5aa9536d2049e79409
Andrew Golightly <support@andrewgolightly.com>
2018-06-13 09:03:04 -0600

    removed logging of number of messages deleted

--- services/periodically-clear-bin-folders.js
+++ services/periodically-clear-bin-folders.js
@@ -7,11 +7,11 @@ module.exports = interval => {
     console.log(`Clearing Trash folders at ${new Date().toString()}`);
     const users = await User.find({ clearBinFolder: true }).exec();
     for (const user of users) {
-      const numMessagesDeleted = await emptyBin(user);
+      await emptyBin(user);
       console.log(
         `Cleared Trash folder for ${user.firstName} ${user.lastName} (${
           user.emailAddress
-        }). Number of messages deleted: ${numMessagesDeleted}`
+        }).`
       );
     }
     console.log('...done');
